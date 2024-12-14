de4778ac0cb34a5975d6cfbff35c808bbe157f88
Joleksiuk <julkaoleksiuk@gmail.com>
2024-12-14 23:24:28 +0100

    changed setup frotnend

--- backend-server/src/middlewares/setupFrontend.ts
+++ backend-server/src/middlewares/setupFrontend.ts
@@ -1,23 +1,23 @@
 import express, { Application } from "express";
 import path from "path";
 
-export function setupFrontend(app: Application): void {
-  const frontendPath = path.join(__dirname, "../../frontend/build");
-  const indexPath = path.join(frontendPath, "index.html");
-
-  app.use(express.static(frontendPath));
-
-  app.get("*", (req, res) => {
-    res.sendFile(indexPath);
-  });
-}
-
 // export function setupFrontend(app: Application): void {
-//   const frontendPath = path.join(__dirname, "../../../frontend/build");
+//   const frontendPath = path.join(__dirname, "../../frontend/build");
+//   const indexPath = path.join(frontendPath, "index.html");
 
 //   app.use(express.static(frontendPath));
 
 //   app.get("*", (req, res) => {
-//     res.sendFile(path.join(frontendPath, "index.html"));
+//     res.sendFile(indexPath);
 //   });
 // }
+
+export function setupFrontend(app: Application): void {
+  const frontendPath = path.join(__dirname, "../../../frontend/build");
+
+  app.use(express.static(frontendPath));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(frontendPath, "index.html"));
+  });
+}
